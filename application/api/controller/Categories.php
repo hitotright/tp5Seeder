bf304d3982d5f1d995723a25f372eef487fd5575
hitoTright <915840223@qq.com>
2017-08-08 23:52:25 +0800

    init

---
+++ application/api/controller/Categories.php
@@ -0,0 +1,10 @@
+<?php
+namespace app\api\controller;
+
+class Categories extends \think\Controller
+{
+    public function index()
+    {
+        return json('categories1');
+    }
+}
---
+++ public/src/components/Home.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <h3>CATEGORIES</h3>
+        <p>{{categories}}</p>
+        <button  v-on:click="changeBash">changeBash</button>
+    </div>
+</template>
+
+<script>
+    export default {
+        mounted () {
+            this.fetchCategories()
+        },
+        data () {
+            return {
+                categories:'categories',
+                title:'home'
+            }
+        },
+        methods: {
+            fetchCategories() {
+                this.$http({url: '/api.php/api/categories', method: 'GET'}).then(function (response) {
+                    this.$set(this,'categories', response.data)
+                })
+            },
+            changeBash: function (event) {
+               alert('ss');
+            }
+        }
+    }
+</script>
