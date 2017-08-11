96696bd20553987ad311fd527f18fc709c1e0bb0
hito <915840223@qq.com>
2017-08-11 14:15:42 +0800

    init

--- template/webpack.base.config.js
+++ template/webpack.base.config.js
@@ -56,7 +56,7 @@ module.exports = {
 
             {
                 test: /\.(gif|jpg|png|woff|svg|eot|ttf)\??.*$/,
-                loader: 'url-loader?limit=1024'
+                loader: 'url-loader?limit=1024&name=images/[name].[hash].[ext]'
             },
             {
                 test: /\.(html|tpl)$/,
